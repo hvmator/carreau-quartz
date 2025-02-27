134558bfc76451e64c23ad82fcb3d1fd35fbd99c
hvmator <bleach.is.in.the.sky@gmail.com>
2025-02-27 14:56:04 -0500

    Quartz sync: Feb 27, 2025, 2:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Project CARREAU",
+    pageTitle: "CARREAU",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
